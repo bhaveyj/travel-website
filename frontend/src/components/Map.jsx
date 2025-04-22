630b462491313d2e8e27547eafd8d6eab7e0be3f
bhavyajoshi <bjoshi1503@gmail.com>
2025-04-22 13:12:49 +0530

    modified map component

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -14,9 +14,9 @@ const WeatherMiniMap = () => {
         fontSize: "20px",
         fontWeight: "600"
       }}>
-        Heatmap
+        Global Heatmap
       </h3>
-      <div style={{ width: "300px", height: "200px", borderRadius: "10px", overflow: "hidden", boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)" }}>
+      <div style={{ width: "362px", height: "285px", borderRadius: "15px", overflow: "hidden", boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)" }}>
         <MapContainer center={[20, 0]} zoom={1.5} style={{ width: "100%", height: "100%" }}>
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
           <TileLayer url={openWeatherTileUrl} />
